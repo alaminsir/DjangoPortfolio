353f69a1f6ec62ff5d7440512bfc839d30f9ea09
alaminsir <alaminsir404@gmail.com>
2024-06-05 23:35:01 +0600

    update some file

--- DjangoPortfolio/urls.py
+++ DjangoPortfolio/urls.py
@@ -3,6 +3,8 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
     path('employee/',include('employee.urls')),
 ]
